b77aa920d7bb8b6881b09e7f345da10ff37eef1c
sj <sebastianthegreatful@gmail.com>
2021-11-12 01:43:18 +0800

    fix: don't git ignore main.go

---
+++ cmd/serve/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/dhis2-sre/go-rate-limite/pgk/di"
+	"log"
+	"net/http"
+)
+
+func main() {
+	app := di.GetApplication()
+
+	port := app.Config.ServerPort
+	handler := http.HandlerFunc(app.Handler.RateLimitingProxyHandler)
+
+	log.Println("Listening on port: " + port)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+}
