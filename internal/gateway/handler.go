60e99024efec4b391650888ed228e55baadacc08
sj <sebastianthegreatful@gmail.com>
2021-11-22 12:21:25 +0800

    fix: faulty token validation

--- internal/gateway/handler.go
+++ internal/gateway/handler.go
@@ -1,30 +1,68 @@
 package gateway
 
 import (
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"errors"
 	"github.com/lestrrat-go/jwx/jwa"
 	"github.com/lestrrat-go/jwx/jwt"
+	"log"
 	"net/http"
 	"strings"
 )
 
 func ProvideHandler(c *Config, router *Router) http.HandlerFunc {
+	publicKey, err := providePublicKey(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	return func(w http.ResponseWriter, req *http.Request) {
 		if match, r := router.match(req); match {
 			if r.Authentication == "jwt" {
-				valid, err := validateRequest(c.Authentication.Jwt.PublicKey, req)
-				if !valid || err != nil {
+				valid, err := validateRequest(publicKey, req)
+				if err != nil {
+					log.Println(err)
+					w.WriteHeader(http.StatusForbidden)
+					return
+				}
+				if !valid {
 					w.WriteHeader(http.StatusForbidden)
 					return
 				}
 			}
-			// TODO: This is only necessary if the service behind this gateway is using the http host header
+			// TODO: This is only necessary if the service behind this gateway is using the http host header... Maybe this should be done differently?
 			fixHost(req, r.Backend)
 			r.Handler.ServeHTTP(w, req)
 			return
 		}
 		w.WriteHeader(http.StatusNotFound)
 	}
+}
+
+func providePublicKey(c *Config) (*rsa.PublicKey, error) {
+	publicKeyString := c.Authentication.Jwt.PublicKey
+	if publicKeyString != "" {
+		decode, _ := pem.Decode([]byte(publicKeyString))
+		publicKey, err := x509.ParsePKIXPublicKey(decode.Bytes)
+		if err != nil {
+			return nil, err
+		}
+		return publicKey.(*rsa.PublicKey), nil
+	}
+	return nil, errors.New("public not configured")
+}
+
+func validateRequest(publicKey *rsa.PublicKey, req *http.Request) (bool, error) {
+	_, err := jwt.ParseRequest(req,
+		jwt.WithValidate(true),
+		jwt.WithVerify(jwa.RS256, publicKey),
+	)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
 }
 
 func fixHost(req *http.Request, ruleBackend string) {
@@ -38,22 +76,4 @@ func fixHost(req *http.Request, ruleBackend string) {
 		}
 		req.Host = backend
 	}
-}
-
-func validateRequest(publicKey string, req *http.Request) (bool, error) {
-	authorizationHeader := req.Header.Get("Authorization")
-	if !strings.HasPrefix(authorizationHeader, "Bearer") {
-		return false, errors.New("no bearer token prefix found in header")
-	}
-	tokenString := strings.TrimPrefix(authorizationHeader, "Bearer")
-
-	_, err := jwt.Parse(
-		[]byte(tokenString),
-		jwt.WithValidate(true),
-		jwt.WithVerify(jwa.RS256, publicKey),
-	)
-	if err != nil {
-		return true, nil
-	}
-	return false, err
 }
