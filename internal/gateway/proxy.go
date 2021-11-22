ca39c50909316eb1110c86403633544c29adaa9f
sj <sebastianthegreatful@gmail.com>
2021-11-22 13:11:54 +0800

    refactor: change request log format

--- internal/gateway/proxy.go
+++ internal/gateway/proxy.go
@@ -9,7 +9,7 @@ import (
 
 func provideTransparentProxy(backendUrl *url.URL) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		log.Printf("%s %s -> %s", req.Method, req.URL.Path, backendUrl)
+		log.Printf("%s %s%s", req.Method, backendUrl, req.URL.Path)
 		proxy := httputil.NewSingleHostReverseProxy(backendUrl)
 		proxy.ServeHTTP(w, req)
 	}
