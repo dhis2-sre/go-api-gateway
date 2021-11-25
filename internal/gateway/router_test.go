5e96b4ab28f680019aafbac843356ad07a746c18
sj <sebastianthegreatful@gmail.com>
2021-11-25 14:12:16 +0800

    test: add test for matching where two rules share the same path prefix and http method but have different headers

--- internal/gateway/router_test.go
+++ internal/gateway/router_test.go
@@ -74,3 +74,47 @@ func TestMatchWithBasePath(t *testing.T) {
 
 	assert.Equal(t, expected, actual)
 }
+
+func TestMatchSamePathAndMethodButDifferentHeaders(t *testing.T) {
+	userAgentKey := "User-Agent"
+
+	headers0 := map[string][]string{
+		userAgentKey: {"Go tests"},
+	}
+	rule0 := &ConfigRule{
+		PathPrefix: "/health",
+		Method:     "GET",
+		Headers:    headers0,
+		Backend:    "backend0",
+	}
+
+	headers1 := map[string][]string{
+		userAgentKey: {"Some other client"},
+	}
+	rule1 := &ConfigRule{
+		PathPrefix: "/health",
+		Method:     "GET",
+		Headers:    headers1,
+		Backend:    "backend1",
+	}
+
+	configRules := []ConfigRule{*rule0, *rule1}
+	c := &Config{Rules: configRules}
+
+	router, err := ProvideRouter(c)
+	assert.NoError(t, err)
+
+	req0, err := http.NewRequest("GET", "/health", nil)
+	assert.NoError(t, err)
+	req0.Header.Set(userAgentKey, headers0[userAgentKey][0])
+	actual, actualRule0 := router.match(req0)
+	assert.Equal(t, true, actual)
+	assert.Equal(t, "backend0", actualRule0.Backend)
+
+	req1, err := http.NewRequest("GET", "/health", nil)
+	assert.NoError(t, err)
+	req1.Header.Set(userAgentKey, headers1[userAgentKey][0])
+	actual, actualRule1 := router.match(req1)
+	assert.Equal(t, true, actual)
+	assert.Equal(t, "backend1", actualRule1.Backend)
+}
