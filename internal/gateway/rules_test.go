7320b95d7d0d0550e8eac22d8b819fba1a8f0b4e
sj <sebastianthegreatful@gmail.com>
2021-12-24 01:28:58 +0800

    test: add more tests for Rules

--- internal/gateway/rules_test.go
+++ internal/gateway/rules_test.go
@@ -33,3 +33,108 @@ func TestMatchUnmappedBackend(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Equal(t, "backend map contains not entry for: some-undefined-backend", err.Error())
 }
+
+func TestLen(t *testing.T) {
+	rule := &ConfigRule{
+		PathPrefix: "/health",
+		Method:     "GET",
+	}
+
+	configRules := []ConfigRule{*rule}
+	c := &Config{DefaultBackend: defaultBackend, Backends: getBackends(), Rules: configRules}
+
+	rules, err := ProvideRules(c)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 1, rules.Len())
+}
+
+func TestLenNoMethod(t *testing.T) {
+	rule := &ConfigRule{
+		PathPrefix: "/health",
+	}
+
+	configRules := []ConfigRule{*rule}
+	c := &Config{DefaultBackend: defaultBackend, Backends: getBackends(), Rules: configRules}
+
+	rules, err := ProvideRules(c)
+	assert.NoError(t, err)
+
+	assert.Equal(t, 9, rules.Len())
+}
+
+func TestLookup(t *testing.T) {
+	rule := &ConfigRule{
+		PathPrefix: "/health",
+		Method:     "GET",
+	}
+
+	configRules := []ConfigRule{*rule}
+	c := &Config{DefaultBackend: defaultBackend, Backends: getBackends(), Rules: configRules}
+
+	r, err := ProvideRules(c)
+	assert.NoError(t, err)
+
+	i, match := r.Lookup([]byte("GET" + rule.PathPrefix))
+	assert.True(t, match)
+
+	rules := i.([]*Rule)
+	assert.Equal(t, rule.PathPrefix, rules[0].PathPrefix)
+	assert.Equal(t, rule.Method, rules[0].Method)
+}
+
+func TestLookupWithTwoRules(t *testing.T) {
+	pathPrefix := "/health"
+	method := "GET"
+
+	ruleA := &ConfigRule{
+		PathPrefix: pathPrefix,
+		Method:     method,
+		Hostname:   "domain.org",
+	}
+
+	ruleB := &ConfigRule{
+		PathPrefix: pathPrefix,
+		Method:     method,
+		Hostname:   "other.domain.org",
+	}
+
+	configRules := []ConfigRule{*ruleA, *ruleB}
+	c := &Config{DefaultBackend: defaultBackend, Backends: getBackends(), Rules: configRules}
+
+	r, err := ProvideRules(c)
+	assert.NoError(t, err)
+
+	i, match := r.Lookup([]byte(method + pathPrefix))
+	assert.True(t, match)
+
+	rules := i.([]*Rule)
+
+	assert.Equal(t, pathPrefix, rules[0].PathPrefix)
+	assert.Equal(t, method, rules[0].Method)
+	assert.Equal(t, ruleB.Hostname, rules[0].Hostname)
+
+	assert.Equal(t, pathPrefix, rules[1].PathPrefix)
+	assert.Equal(t, method, rules[1].Method)
+	assert.Equal(t, ruleA.Hostname, rules[1].Hostname)
+}
+
+func TestWalk(t *testing.T) {
+	rule := &ConfigRule{
+		PathPrefix: "/health",
+		Method:     "GET",
+	}
+
+	configRules := []ConfigRule{*rule}
+	c := &Config{DefaultBackend: defaultBackend, Backends: getBackends(), Rules: configRules}
+
+	rules, err := ProvideRules(c)
+	assert.NoError(t, err)
+
+	rules.Walk(func(v interface{}) bool {
+		r := v.([]*Rule)[0]
+		assert.Equal(t, rule.PathPrefix, r.PathPrefix)
+		assert.Equal(t, rule.Method, r.Method)
+		return false
+	})
+}
